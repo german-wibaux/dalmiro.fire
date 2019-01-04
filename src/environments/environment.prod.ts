fdfda07b4a0b27ff07fd851fae5712e648526542
Yerman <germanwibaux@gmail.com>
2019-01-04 12:52:58 -0300

    Fix environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     firebaseConfig : {apiKey: "AIzaSyDL2RdJRnSGKzuEL9TaI6KKDr1g2UZLDAc",
-  authDomain: "dalmiro-app.firebaseapp.com",
+  authDomain: "dalmirosuarezpisos.com.ar",
   databaseURL: "https://dalmiro-app.firebaseio.com",
   projectId: "dalmiro-app",
   storageBucket: "dalmiro-app.appspot.com",
