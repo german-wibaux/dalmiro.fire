e26474be8cd63af684ed599cafc56f80eca74f34
Yerman <germanwibaux@gmail.com>
2019-01-03 21:09:54 -0300

    List articles found

--- src/app/components/melamina/melamina.component.ts
+++ src/app/components/melamina/melamina.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from 'src/app/services/article.service';
+import { ArticleInterface } from 'src/app/models/articleInterface';
 
 @Component({
   selector: 'app-melamina',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MelaminaComponent implements OnInit {
 
-  constructor() { }
+  articles:  ArticleInterface[] = [];
+
+  constructor(private articleService: ArticleService) { 
+    this.articleService.getArticles().subscribe( articles => {
+      /** List every properties */
+      //console.log(articles);
+      articles.forEach(element => {
+        if (element.category == 'melaminico') {
+          this.articles.push(element);
+        }
+      });
+          
+    });
+  }
 
   ngOnInit() {
   }
